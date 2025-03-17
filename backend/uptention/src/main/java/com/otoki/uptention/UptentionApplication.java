4c1bf37766791050c9f684e80b707acadf8b3a04
조대성 <tfer2442@naver.com>
2025-03-17 21:02:58 +0900

    chore: 프로젝트 생성

---
+++ backend/uptention/src/main/java/com/otoki/uptention/UptentionApplication.java
@@ -0,0 +1,13 @@
+package com.otoki.uptention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptentionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptentionApplication.class, args);
+	}
+
+}
---
+++ backend/uptention/src/test/java/com/otoki/uptention/UptentionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otoki.uptention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
