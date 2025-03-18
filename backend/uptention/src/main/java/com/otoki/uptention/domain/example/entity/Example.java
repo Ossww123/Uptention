37b8666d6bb2b1e3eb36e286669226aca3e7c711
조대성 <tfer2442@naver.com>
2025-03-18 19:26:58 +0900

    feat: 예제 코드에 도메인 서비스 계층 추가 (S12P21D211-144)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/example/entity/Example.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/example/entity/Example.java
@@ -1,11 +1,28 @@
 package com.otoki.uptention.domain.example.entity;
 
-// @Entity
-// @EntityListeners(AuditingEntityListener.class)
-// @AllArgsConstructor
-// @NoArgsConstructor(access = AccessLevel.PROTECTED)
-// @Builder
-// @Getter
-// @Setter
-// public class Example {
-// }
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@Setter
+public class Example {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+}
--- backend/uptention/src/main/java/com/otoki/uptention/domain/example/service/ExampleService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/example/service/ExampleService.java
@@ -1,4 +1,4 @@
 package com.otoki.uptention.domain.example.service;
 
-// public interface ExampleService {
-// }
+public interface ExampleService {
+}
