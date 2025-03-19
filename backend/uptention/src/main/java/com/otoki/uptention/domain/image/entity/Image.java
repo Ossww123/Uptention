86c8ab24d48c344e272d99c66e28446866cbe211
ssuzyn <asj7090@gmail.com>
2025-03-20 01:19:06 +0900

    feat: 마켓 상품 entity 관계 추가 (S12P21D211-120)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/image/entity/Image.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/image/entity/Image.java
@@ -1,12 +1,16 @@
 package com.otoki.uptention.domain.image.entity;
 
+import com.otoki.uptention.domain.item.entity.Item;
 import com.otoki.uptention.global.entity.TimeStampEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -27,4 +31,8 @@ public class Image extends TimeStampEntity {
 
 	@Column(name = "url", length = 127)
 	private String url;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	private Item item;
 }
