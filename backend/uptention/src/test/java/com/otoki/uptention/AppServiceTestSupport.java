b57c88177351471c6c5f5f9c4f90a383947c555f
asj7090 <asj7090@gmail.com>
2025-03-20 10:17:42 +0900

    fix: facade service layer 지원 클래스로 수정 (S12P21D211-120)

--- backend/uptention/src/test/java/com/otoki/uptention/AppServiceTestSupport.java
+++ backend/uptention/src/test/java/com/otoki/uptention/AppServiceTestSupport.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 /**
- * Service 레이어 테스트용 기본 지원 클래스.
+ * Application Service 레이어 테스트용 기본 지원 클래스.
  *
  * - {@link SpringBootTest} 적용: 애플리케이션 컨텍스트 로드하여 서비스 계층 테스트 가능.
  * - {@link ActiveProfiles}("test") 적용: 테스트 환경에서는 "test" 프로필 사용.
@@ -12,5 +12,5 @@ import org.springframework.test.context.ActiveProfiles;
  */
 @ActiveProfiles("test")
 @SpringBootTest
-public abstract class ServiceTestSupport {
+public abstract class AppServiceTestSupport {
 }
