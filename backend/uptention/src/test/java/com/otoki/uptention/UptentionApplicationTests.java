ea4989521069d66b769144f9f03ab7876ed54029
ssuzyn <asj7090@gmail.com>
2025-03-22 01:45:26 +0900

    comment: @SpringBootTest 주석 해제

--- backend/uptention/src/test/java/com/otoki/uptention/UptentionApplicationTests.java
+++ backend/uptention/src/test/java/com/otoki/uptention/UptentionApplicationTests.java
@@ -1,9 +1,10 @@
 package com.otoki.uptention;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-// @SpringBootTest(classes = UptentionApplication.class) // main 애플리케이션 클래스 지정
+@SpringBootTest(classes = UptentionApplication.class) // main 애플리케이션 클래스 지정
 @ActiveProfiles("test") // 테스트 프로파일 적용
 class UptentionApplicationTests {
 
