3e40fa9ea7a91f603bccdef712e338c05c0005c8
ssuzyn <asj7090@gmail.com>
2025-03-22 02:23:46 +0900

    fix: 테스트 환경의 데이터베이스 구성 수정

--- backend/uptention/src/test/java/com/otoki/uptention/RepositoryTestSupport.java
+++ backend/uptention/src/test/java/com/otoki/uptention/RepositoryTestSupport.java
@@ -1,6 +1,5 @@
 package com.otoki.uptention;
 
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,11 +11,9 @@ import com.otoki.uptention.global.config.QuerydslConfig;
  *
  * - {@link DataJpaTest} 적용: JPA 관련 테스트만 수행하도록 설정.
  * - {@link ActiveProfiles}("test") 적용: 테스트 환경에서는 "test" 프로필 사용.
- * - {@link AutoConfigureTestDatabase}(replace = NONE): 내장 DB 대신 별도 테스트 DB 사용.
  */
 @DataJpaTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(QuerydslConfig.class)
 public abstract class RepositoryTestSupport {
 }
