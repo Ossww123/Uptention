f7efed6a3eb9e5b074cbf7c2108e4a7e6b66788c
ssuzyn <asj7090@gmail.com>
2025-03-23 00:30:01 +0900

    feat: 주문 내역 저장 기능 추가 (S12P21D211-152)

---
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/order/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.otoki.uptention.domain.order.service;
+
+import com.otoki.uptention.domain.order.entity.Order;
+
+public interface OrderService {
+	Order createOrderPurchase(Order order);
+}
