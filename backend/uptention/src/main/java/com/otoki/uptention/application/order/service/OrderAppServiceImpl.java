84b921759e11bc08a163f0ae3032e557ca678285
ssuzyn <asj7090@gmail.com>
2025-03-24 03:24:49 +0900

    feat: 선물 주문 facade service 구현(S12P21D211-149)

--- backend/uptention/src/main/java/com/otoki/uptention/application/order/service/OrderAppServiceImpl.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/order/service/OrderAppServiceImpl.java
@@ -3,11 +3,14 @@ package com.otoki.uptention.application.order.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.otoki.uptention.application.order.dto.request.GiftRequestDto;
 import com.otoki.uptention.application.order.dto.request.ItemQuantityRequestDto;
 import com.otoki.uptention.application.order.dto.request.OrderRequestDto;
 import com.otoki.uptention.domain.item.entity.Item;
 import com.otoki.uptention.domain.item.service.ItemService;
+import com.otoki.uptention.domain.order.entity.Gift;
 import com.otoki.uptention.domain.order.entity.Order;
+import com.otoki.uptention.domain.order.service.GiftService;
 import com.otoki.uptention.domain.order.service.OrderService;
 import com.otoki.uptention.domain.orderitem.entity.OrderItem;
 import com.otoki.uptention.domain.orderitem.service.OrderItemService;
@@ -25,6 +28,7 @@ public class OrderAppServiceImpl implements OrderAppService{
 	private final OrderItemService orderItemService;
 	private final ItemService itemService;
 	private final UserService userService;
+	private final GiftService giftService;
 
 	@Transactional
 	@Override
@@ -55,6 +59,45 @@ public class OrderAppServiceImpl implements OrderAppService{
 			// 판매량 증가
 			item.increaseSalesCount(itemRequest.getQuantity());
 		}
+		return savedOrder;
+	}
+
+	@Transactional
+	@Override
+	public Order createGiftOrder(GiftRequestDto giftRequestDto) {
+		// security 구현 후, 코드 수정 필요
+		User sender = userService.getUserById(2);
+		User receiver = userService.getUserById(giftRequestDto.getReceiverId());
+
+		// 1. Order 생성 - 선물의 경우 주소 X
+		Order order = Order.builder()
+			.user(sender) // 선물을 보내는 사람(구매자)
+			.build();
+		Order savedOrder = orderService.createOrderPurchase(order);
+
+		// 2. OrderItem 생성 및 저장
+		Item item = itemService.getItemDetails(giftRequestDto.getItemId());
+
+		OrderItem orderItem = OrderItem.builder()
+			.order(order)
+			.item(item)
+			.quantity(1) // 선물은 기본적으로 수량 1개
+			.itemPrice(item.getPrice())
+			.build();
+
+		orderItemService.createOrderItem(orderItem);
+
+		// 판매량 증가
+		item.increaseSalesCount(1);
+
+		// 3. Gift 엔티티 생성
+		Gift gift = Gift.builder()
+			.order(order)
+			.receiver(receiver)
+			.build();
+
+		giftService.createOrderGift(gift);
+
 		return savedOrder;
 	}
 }
