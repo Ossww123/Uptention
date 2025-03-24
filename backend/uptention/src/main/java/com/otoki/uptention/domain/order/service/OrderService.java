3fe3507b20b5b63765f28e36ee09e1f4f60af004
ssuzyn <asj7090@gmail.com>
2025-03-25 02:36:07 +0900

    feat: 주문 조회 서비스 메서드 추가 (S12P21D211-186)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/order/service/OrderService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/order/service/OrderService.java
@@ -4,4 +4,5 @@ import com.otoki.uptention.domain.order.entity.Order;
 
 public interface OrderService {
 	Order save(Order order);
+	Order getOrderById(Integer id);
 }
--- backend/uptention/src/main/java/com/otoki/uptention/domain/order/service/OrderServiceImpl.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/order/service/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.otoki.uptention.domain.order.entity.Order;
 import com.otoki.uptention.domain.order.repository.OrderRepository;
+import com.otoki.uptention.global.exception.CustomException;
+import com.otoki.uptention.global.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,5 +18,11 @@ public class OrderServiceImpl implements OrderService {
 	@Override
 	public Order save(Order order) {
 		return orderRepository.save(order);
+	}
+
+	@Override
+	public Order getOrderById(Integer id) {
+		return orderRepository.findById(id)
+			.orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
 	}
 }
