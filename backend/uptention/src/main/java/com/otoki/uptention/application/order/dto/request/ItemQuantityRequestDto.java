6830f4b2fa8ec5df118c00872fabac02bd59a9b1
ssuzyn <asj7090@gmail.com>
2025-03-25 01:30:09 +0900

    docs: 상품 구매/선물 요청 DTO swagger 적용 (S12P21D211-186)

--- backend/uptention/src/main/java/com/otoki/uptention/application/order/dto/request/ItemQuantityRequestDto.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/order/dto/request/ItemQuantityRequestDto.java
@@ -1,5 +1,6 @@
 package com.otoki.uptention.application.order.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,7 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "상품 수량 요청 DTO")
 public class ItemQuantityRequestDto {
+
+	@Schema(description = "상품 ID", example = "1", required = true)
 	private Integer itemId;
+
+	@Schema(description = "상품 수량", example = "1", required = true)
 	private Integer quantity;
 }
