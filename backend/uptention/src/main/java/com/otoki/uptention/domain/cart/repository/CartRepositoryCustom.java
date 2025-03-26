5faa58c15243ef2d9c8f3c700c35c9d929fed032
asj7090 <asj7090@gmail.com>
2025-03-26 16:10:18 +0900

    feat: 사용자별 장바구니 항목 조회 기능 구현 (S12P21D211-182)

---
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/cart/repository/CartRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.otoki.uptention.domain.cart.repository;
+
+import java.util.List;
+
+import com.otoki.uptention.domain.cart.dto.CartItemDto;
+
+public interface CartRepositoryCustom {
+	List<CartItemDto> findCartItemsByUserId(Integer userId);
+
+	List<CartItemDto> findCartItemsByIds(Integer userId, List<Integer> cartIds);
+}
