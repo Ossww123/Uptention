7763b9246f5ab6c5f6fe6785103c191d99115b2c
ssuzyn <asj7090@gmail.com>
2025-03-28 00:18:59 +0900

    feat: 주문 상품 조회 repository 메서드 구현 (S12P21D211-164)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/orderitem/repository/OrderItemRepository.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/orderitem/repository/OrderItemRepository.java
@@ -1,10 +1,20 @@
 package com.otoki.uptention.domain.orderitem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.otoki.uptention.domain.orderitem.entity.OrderItem;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+	@Query("SELECT oi FROM OrderItem oi " +
+		"JOIN FETCH oi.item " +
+		"JOIN FETCH oi.order " +
+		"WHERE oi.order.id IN :orderIds " +
+		"ORDER BY oi.order.createdAt DESC, oi.order.id DESC")
+	List<OrderItem> findAllByOrderIdInWithItemJoin(@Param("orderIds") List<Integer> orderIds);
 }
