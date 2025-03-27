617eafbdc33d35f627bb33f01cd0ac083105cbc2
류재문 <woans5970@naver.com>
2025-03-27 22:03:41 +0900

    fix: 집중모드 시작 기능 로직 수정 (S12P21D211-208)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/mining/service/MiningTimeServiceImpl.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/mining/service/MiningTimeServiceImpl.java
@@ -17,12 +17,7 @@ public class MiningTimeServiceImpl implements MiningTimeService {
 
 	@Override
 	public void saveMiningTime(MiningTime miningTime) {
-		try {
-			// 채굴시간 저장
-			miningTimeRepository.save(miningTime);
-		} catch (Exception e) {
-			// 저장 실패 시 커스텀 오류 생성
-			throw new CustomException(ErrorCode.FOCUS_MODE_ON_FAILED);
-		}
+		// 채굴시간 저장
+		miningTimeRepository.save(miningTime);
 	}
 }
