b3549c05cbdccbe998bd3a78669d9e19f8513d4a
ssuzyn <asj7090@gmail.com>
2025-03-28 00:46:09 +0900

    feat: 구매 내역 목록 조회 API 구현 (S12P21D211-164)

--- backend/uptention/src/main/java/com/otoki/uptention/presentation/order/controller/OrderController.java
+++ backend/uptention/src/main/java/com/otoki/uptention/presentation/order/controller/OrderController.java
@@ -3,10 +3,12 @@ package com.otoki.uptention.presentation.order.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.otoki.uptention.application.order.dto.request.DeliveryInfoRequestDto;
@@ -14,6 +16,7 @@ import com.otoki.uptention.application.order.dto.request.GiftRequestDto;
 import com.otoki.uptention.application.order.dto.request.ItemVerificationDto;
 import com.otoki.uptention.application.order.dto.request.OrderRequestDto;
 import com.otoki.uptention.application.order.dto.response.ItemVerificationResponseDto;
+import com.otoki.uptention.application.order.dto.response.OrderHistoryCursorResponseDto;
 import com.otoki.uptention.application.order.service.OrderAppService;
 import com.otoki.uptention.application.order.service.OrderVerifyAppService;
 import com.otoki.uptention.presentation.order.doc.OrderApiDoc;
@@ -52,6 +55,14 @@ public class OrderController implements OrderApiDoc {
 		@Valid @RequestBody DeliveryInfoRequestDto deliveryInfoRequestDto) {
 		orderAppService.registerDeliveryInfo(orderId, deliveryInfoRequestDto);
 		return ResponseEntity.ok("배송지 정보 등록 성공");
+	}
+
+	@GetMapping("")
+	public ResponseEntity<OrderHistoryCursorResponseDto> getOrderHistory(
+		@RequestParam(required = false) String cursor,
+		@RequestParam(defaultValue = "10") int size,
+		@RequestParam(defaultValue = "PURCHASE") String type) {
+		return ResponseEntity.ok(orderAppService.getOrderHistory(cursor, size, type));
 	}
 
 }
