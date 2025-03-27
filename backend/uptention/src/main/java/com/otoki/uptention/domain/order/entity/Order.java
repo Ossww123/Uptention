55708d3c5cb41524ad20fea24ff03fff0bf6ac67
asj7090 <asj7090@gmail.com>
2025-03-27 17:13:34 +0900

    feat: Order 엔티티에 주문 상태 필드 추가 (S12P21D211-182)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/order/entity/Order.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/order/entity/Order.java
@@ -1,11 +1,14 @@
 package com.otoki.uptention.domain.order.entity;
 
+import com.otoki.uptention.domain.order.enums.OrderStatus;
 import com.otoki.uptention.domain.user.entity.User;
 import com.otoki.uptention.global.entity.TimeStampEntity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -39,12 +42,23 @@ public class Order extends TimeStampEntity {
 	@Column(length = 127)
 	private String address;
 
+	@Builder.Default
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20, nullable = false)
+	private OrderStatus status = OrderStatus.PAYMENT_PENDING;
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Gift gift;
 
 	// 배송지 정보를 설정하거나 수정
-	public Order updateAddress(String address){
+	public Order updateAddress(String address) {
 		this.address = address;
+		return this;
+	}
+
+	// 주문 상태 업데이트
+	public Order updateStatus(OrderStatus status) {
+		this.status = status;
 		return this;
 	}
 }
