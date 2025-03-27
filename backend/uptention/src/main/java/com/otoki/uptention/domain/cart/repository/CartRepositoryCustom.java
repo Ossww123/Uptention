1db31e76b8059b6386997864410ff87afcaf99f7
ssuzyn <asj7090@gmail.com>
2025-03-28 01:22:00 +0900

    remove: 사용하지 않는 클래스 삭제 (S12P21D211-164)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/cart/repository/CartRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.otoki.uptention.domain.cart.repository;
-
-import java.util.List;
-
-import com.otoki.uptention.domain.cart.dto.CartItemDto;
-
-public interface CartRepositoryCustom {
-	List<CartItemDto> findCartItemsByUserId(Integer userId);
-}
