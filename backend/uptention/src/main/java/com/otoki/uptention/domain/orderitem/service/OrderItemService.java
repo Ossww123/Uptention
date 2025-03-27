c177e373c18a5761d9547b628df95bda4b9e2b1e
ssuzyn <asj7090@gmail.com>
2025-03-28 00:20:54 +0900

    feat: 주문 상품 조회 메서드 구현 (S12P21D211-164)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/orderitem/service/OrderItemService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/orderitem/service/OrderItemService.java
@@ -1,8 +1,11 @@
 package com.otoki.uptention.domain.orderitem.service;
 
+import java.util.List;
+
 import com.otoki.uptention.domain.orderitem.entity.OrderItem;
 
 public interface OrderItemService {
 
 	OrderItem saveOrderItem(OrderItem orderItem);
+	List<OrderItem> findOrderItemsByOrderIds(List<Integer> orderIds);
 }
