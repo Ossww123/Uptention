e8cafac753fc5267dac5c43ccdc3ad3297a19b3b
류재문 <woans5970@naver.com>
2025-03-28 15:34:46 +0900

    feat: 가장 최근 집중모드 조회 기능 추가 (S12P21D211-214)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/mining/repository/MiningTimeRepository.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/mining/repository/MiningTimeRepository.java
@@ -1,10 +1,15 @@
 package com.otoki.uptention.domain.mining.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.otoki.uptention.domain.mining.entity.MiningTime;
+import com.otoki.uptention.domain.user.entity.User;
 
 @Repository
 public interface MiningTimeRepository extends JpaRepository<MiningTime, Integer> {
+	Optional<MiningTime> findTopByUserOrderByStartTimeDesc(User user);
+
 }
