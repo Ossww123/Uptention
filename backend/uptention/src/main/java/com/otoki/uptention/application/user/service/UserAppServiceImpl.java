456d388e2bf3b5901dd1dc070681906dc85aa45f
조대성 <tfer2442@naver.com>
2025-03-29 03:08:58 +0900

    feat: Spring Security가 추가됨에 따라 회원가입 로직 수정 (S12P21D211-74)

--- backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppServiceImpl.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.otoki.uptention.application.user.dto.request.JoinRequestDto;
+import com.otoki.uptention.auth.service.SecurityService;
 import com.otoki.uptention.domain.user.entity.User;
+import com.otoki.uptention.domain.user.enums.UserRole;
 import com.otoki.uptention.domain.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,17 +17,22 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class UserAppServiceImpl implements UserAppService {
 	private final UserService userService;
+	private final SecurityService securityService;
 	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	@Transactional
-	public void joinUser(JoinRequestDto requestDto) {
+	public void joinMember(JoinRequestDto requestDto) { // 일반 멤버 회원가입
+		User admin = securityService.getLoggedInUser(); // 관리자 정보 조회
+
 		// DTO를 도메인 객체(User)로 변환합니다.
 		User user = User.builder()
 			.username(requestDto.getUsername())
 			.password(passwordEncoder.encode(requestDto.getPassword()))
 			.name(requestDto.getName())
 			.employeeNumber(requestDto.getEmployeeNumber())
+			.company(admin.getCompany()) // 관리자와 같은 회사로 연결
+			.role(UserRole.ROLE_TEMP_MEMBER) // 지갑 연결 안된 사용자
 			.build();
 
 		userService.join(user);
