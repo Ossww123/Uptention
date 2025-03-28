93dad0b9fa103c78015861a1da1b04cc6bd3c613
류재문 <woans5970@naver.com>
2025-03-28 15:54:15 +0900

    feat: MiningTimeService end time이 null인 채굴시간 조회 기능 추가 (S12P21D211-214)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/mining/service/MiningTimeService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/mining/service/MiningTimeService.java
@@ -1,5 +1,7 @@
 package com.otoki.uptention.domain.mining.service;
 
+import java.util.List;
+
 import com.otoki.uptention.domain.mining.entity.MiningTime;
 import com.otoki.uptention.domain.user.entity.User;
 
@@ -7,4 +9,5 @@ public interface MiningTimeService {
 
 	void saveMiningTime(MiningTime miningTime);
 	MiningTime findMiningTime(User user);
+	List<MiningTime> findAllByEndTimeIsNull();
 }
