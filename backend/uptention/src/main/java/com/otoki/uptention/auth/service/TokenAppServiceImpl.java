03bf8df46bc8de9e93c027c073eb6860b29f7e48
조대성 <tfer2442@naver.com>
2025-03-29 02:54:45 +0900

    feat: AccessToken 발급 관련 Service 작성 (S12P21D211-74)

---
+++ backend/uptention/src/main/java/com/otoki/uptention/auth/service/TokenAppServiceImpl.java
@@ -0,0 +1,24 @@
+package com.otoki.uptention.auth.service;
+
+import org.springframework.stereotype.Service;
+
+import com.otoki.uptention.auth.constant.JWTConstants;
+import com.otoki.uptention.auth.util.JWTUtil;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TokenAppServiceImpl implements TokenAppService {
+	private final JWTUtil jwtUtil;
+
+	// AccessToken 발급
+	@Override
+	public void issueToken(HttpServletResponse response, Integer userId, String role) {
+		String accessToken = jwtUtil.createJwt(JWTConstants.ACCESS_TOKEN, userId, role,
+			JWTConstants.ACCESS_TOKEN_EXPIRATION_TIME_MS); // AccessToken 만료시간 30일
+
+		response.setHeader(JWTConstants.ACCESS_TOKEN, JWTConstants.PREFIX_JWT + accessToken);
+	}
+}
