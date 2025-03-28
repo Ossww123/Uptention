e22490341a0181d123392fd0098067b2effab03e
류재문 <woans5970@naver.com>
2025-03-28 23:09:16 +0900

    fix: MiningTimeRepository end time 일괄 업데이트 함수 -> 벌크 업데이트 함수로 변경 (S12P21D211-214)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/mining/repository/MiningTimeRepository.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/mining/repository/MiningTimeRepository.java
@@ -1,9 +1,12 @@
 package com.otoki.uptention.domain.mining.repository;
 
-import java.util.List;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.otoki.uptention.domain.mining.entity.MiningTime;
@@ -13,5 +16,8 @@ import com.otoki.uptention.domain.user.entity.User;
 public interface MiningTimeRepository extends JpaRepository<MiningTime, Integer> {
 	Optional<MiningTime> findTopByUserOrderByStartTimeDesc(User user);
 
-	List<MiningTime> findAllByEndTimeIsNull();
+	@Modifying(clearAutomatically = true)
+	@Query("UPDATE MiningTime m SET m.endTime = :endTime WHERE m.endTime IS NULL")
+	int updateEndTimeForUnfinishedMining(@Param("endTime") LocalDateTime endTime);
+
 }
