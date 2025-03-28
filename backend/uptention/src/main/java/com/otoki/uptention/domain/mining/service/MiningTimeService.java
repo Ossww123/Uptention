cdae5b736f8d2ce79017ecc7442c574f80c23fcf
류재문 <woans5970@naver.com>
2025-03-28 15:38:54 +0900

    feat: MiningTimeService 가장 최근 채굴시간 조회 함수 추가 (S12P21D211-214)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/mining/service/MiningTimeService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/mining/service/MiningTimeService.java
@@ -1,8 +1,10 @@
 package com.otoki.uptention.domain.mining.service;
 
 import com.otoki.uptention.domain.mining.entity.MiningTime;
+import com.otoki.uptention.domain.user.entity.User;
 
 public interface MiningTimeService {
 
 	void saveMiningTime(MiningTime miningTime);
+	MiningTime findMiningTime(User user);
 }
