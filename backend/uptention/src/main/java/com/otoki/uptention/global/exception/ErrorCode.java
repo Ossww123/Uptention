64d0d60a6bfdfa7fa6451f7c5e82bf5346e12aa9
조대성 <tfer2442@naver.com>
2025-03-29 02:51:49 +0900

    feat: Auth 관련 에러 코드 추가 (S12P21D211-74)

--- backend/uptention/src/main/java/com/otoki/uptention/global/exception/ErrorCode.java
+++ backend/uptention/src/main/java/com/otoki/uptention/global/exception/ErrorCode.java
@@ -10,16 +10,13 @@ public enum ErrorCode {
 	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "X002", "잘못된 파라미터가 전달되었습니다."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "X003", "서버 에러가 발생했습니다."),
 
-	// 인증/인가 관련 에러
-	// // 401 Unauthorized
-	// {
-	// 	"errorCode": "AUTH_004",
-	// 	"message": "아이디/비밀번호가 일치하지 않습니다."
-	// }
-	AUTH_FAILED_LOGIN(HttpStatus.UNAUTHORIZED, "AUTH_004", "아이디/비밀번호가 일치하지 않습니다."),
+	// AUTH 관련 에러
+	EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_001", "액세스토큰이 만료되었거나 유효하지 않습니다."),
+	NOT_AUTHENTICATED_USER(HttpStatus.UNAUTHORIZED, "AUTH_002", "인증 되지 않은 사용자입니다."),
+	AUTH_FAILED_LOGIN(HttpStatus.UNAUTHORIZED, "AUTH_004", "아이디, 비밀번호가 일치하지 않습니다."),
 	AUTH_DUPLICATE_EMPLOYEE_NUMBER(HttpStatus.CONFLICT, "AUTH_005", "사번이 이미 사용 중입니다."),
 	AUTH_DUPLICATE_USERNAME(HttpStatus.CONFLICT, "AUTH_006", "아이디가 이미 사용중입니다."),
-
+	
 	// 아이템 관련 에러
 	ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "ITEM_001", "상품이 존재하지 않습니다."),
 	ITEM_INVALID_SORT_TYPE(HttpStatus.BAD_REQUEST, "ITEM_002", "지원하지 않는 정렬 방식입니다."),
