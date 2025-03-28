a0aee1ba1435dc1a520458cc75a94d40a87d5189
류재문 <woans5970@naver.com>
2025-03-28 15:46:06 +0900

    feat: MiningTimeAppServiceImpl 채굴시간 종료 함수 구현 (S12P21D211-214)

--- backend/uptention/src/main/java/com/otoki/uptention/application/mining/service/MiningTimeAppServiceImpl.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/mining/service/MiningTimeAppServiceImpl.java
@@ -9,6 +9,8 @@ import com.otoki.uptention.domain.mining.entity.MiningTime;
 import com.otoki.uptention.domain.mining.service.MiningTimeService;
 import com.otoki.uptention.domain.user.entity.User;
 import com.otoki.uptention.domain.user.service.UserService;
+import com.otoki.uptention.global.exception.CustomException;
+import com.otoki.uptention.global.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -36,4 +38,29 @@ public class MiningTimeAppServiceImpl implements MiningTimeAppService {
 		// 2. 채굴시간 저장
 		miningTimeService.saveMiningTime(miningTime);
 	}
+
+	@Transactional
+	@Override
+	public void focusModeOff(Integer userId) {
+		User findUser = userService.getUserById(userId);
+
+		// 1. 채굴 시간 조회
+		MiningTime findMiningTime = miningTimeService.findMiningTime(findUser);
+
+		// 2. 현재 시간 및 기준 시간 지정
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime standardTime = LocalDateTime.now().toLocalDate().atTime(23, 30);
+
+		if (findMiningTime.getEndTime() != null) {
+			throw new CustomException(ErrorCode.FOCUS_MODE_OFF_FAILED);
+		}
+
+		if (now.isAfter(standardTime)) {
+			throw new CustomException(ErrorCode.FOCUS_MODE_INSPECTION);
+		}
+
+		findMiningTime.updateEndTime(now);
+		miningTimeService.saveMiningTime(findMiningTime);
+	}
+
 }
