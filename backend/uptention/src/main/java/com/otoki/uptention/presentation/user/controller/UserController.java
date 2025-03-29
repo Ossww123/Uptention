ac50e7cb58a48bc881d8c1cb94331a64a259e52d
조대성 <tfer2442@naver.com>
2025-03-30 02:39:14 +0900

    feat: 유저 프로필 이미지 삭제 컨트롤러 구현 (S12P21D211-171)

--- backend/uptention/src/main/java/com/otoki/uptention/presentation/user/controller/UserController.java
+++ backend/uptention/src/main/java/com/otoki/uptention/presentation/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.otoki.uptention.presentation.user.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +19,17 @@ import lombok.RequiredArgsConstructor;
 public class UserController implements UserApiDoc {
 	private final UserAppService userAppService;
 
+	// 프로필 이미지 등록 및 수정
 	@PutMapping(value = "/api/users/{userId}/profiles", consumes = "multipart/form-data")
 	public ResponseEntity<ProfileImageResponseDto> updateProfileImage(
 		@PathVariable Integer userId,
 		@RequestParam("profileImage") MultipartFile profileImage) {
-		ProfileImageResponseDto profileImageResponseDto = userAppService.updateProfileImage(userId, profileImage);
+		return ResponseEntity.ok(userAppService.updateProfileImage(userId, profileImage));
+	}
 
-		return ResponseEntity.ok(profileImageResponseDto);
+	// 기본 프로필 이미지로 변경
+	@DeleteMapping("/api/users/{userId}/profiles")
+	public ResponseEntity<ProfileImageResponseDto> deleteProfileImage(@PathVariable Integer userId) {
+		return ResponseEntity.ok(userAppService.removeProfileImage(userId));
 	}
 }
