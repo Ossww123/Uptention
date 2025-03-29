3332fecebd4a7afae5e544559eb1f0efb99de971
5yrotciv <osw9921@gmail.com>
2025-03-30 02:27:16 +0900

    feat: 스플래시 화면 SplashScreen.js  추가 (S12P21D211-96)

---
+++ frontend_app/src/screens/SplashScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Image, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SplashScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar translucent backgroundColor="transparent" />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../../assets/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
+
+export default SplashScreen;
