a1c471a2af6425928886f6d967bac43a6e48ce25
5yrotciv <osw9921@gmail.com>
2025-03-29 14:29:17 +0900

    feat: StackNavigator.js에 알림 스크린 추가 (S12P21D211-193)

--- frontend_app/src/navigations/StackNavigator.js
+++ frontend_app/src/navigations/StackNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabNavigator from './BottomTabNavigator';
 import FocusModeScreen from '../screens/FocusModeScreen';
+import NotificationScreen from '../screens/NotificationScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,8 +19,17 @@ const StackNavigator = () => {
         component={FocusModeScreen} 
         options={{ headerShown: false }}
       />
+      <Stack.Screen 
+        name="Notification" 
+        component={NotificationScreen} 
+        options={{
+          headerShown: false,
+          presentation: 'card',
+          animation: 'slide_from_right'
+        }}
+      />
     </Stack.Navigator>
   );
 };
 
-export default StackNavigator; 
+export default StackNavigator;
