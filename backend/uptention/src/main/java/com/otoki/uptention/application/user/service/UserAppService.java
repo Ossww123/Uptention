98684a238316d11b18dafd0a135cfc0013817a52
조대성 <tfer2442@naver.com>
2025-03-30 02:10:08 +0900

    feat: 프로필 이미지 업로드 Applicaiton Service 작성 (S12P21D211-171)

--- backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppService.java
@@ -1,6 +1,9 @@
 package com.otoki.uptention.application.user.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.otoki.uptention.application.user.dto.request.JoinRequestDto;
+import com.otoki.uptention.application.user.dto.response.ProfileImageResponseDto;
 
 public interface UserAppService {
 	void joinMember(JoinRequestDto requestDto);
@@ -8,4 +11,6 @@ public interface UserAppService {
 	void checkDuplicateUsername(String username);
 
 	void checkDuplicateEmployeeNumber(String employeeNumber);
+
+	ProfileImageResponseDto updateProfileImage(Integer userId, MultipartFile profileImage);
 }
--- backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppServiceImpl.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppServiceImpl.java
@@ -3,12 +3,17 @@ package com.otoki.uptention.application.user.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.otoki.uptention.application.user.dto.request.JoinRequestDto;
+import com.otoki.uptention.application.user.dto.response.ProfileImageResponseDto;
 import com.otoki.uptention.auth.service.SecurityService;
 import com.otoki.uptention.domain.user.entity.User;
 import com.otoki.uptention.domain.user.enums.UserRole;
 import com.otoki.uptention.domain.user.service.UserService;
+import com.otoki.uptention.global.exception.CustomException;
+import com.otoki.uptention.global.exception.ErrorCode;
+import com.otoki.uptention.global.service.ImageUploadService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class UserAppServiceImpl implements UserAppService {
 	private final UserService userService;
 	private final SecurityService securityService;
+	private final ImageUploadService imageUploadService;
 	private final PasswordEncoder passwordEncoder;
 
 	@Override
@@ -48,5 +54,24 @@ public class UserAppServiceImpl implements UserAppService {
 	@Override
 	public void checkDuplicateEmployeeNumber(String employeeNumber) {
 		userService.validateDuplicateEmployeeNumber(employeeNumber);
+	}
+
+	// 프로필 이미지 업로드/업데이트
+	@Override
+	@Transactional
+	public ProfileImageResponseDto updateProfileImage(Integer userId, MultipartFile profileImage) {
+		User user = securityService.getLoggedInUser();
+
+		// 로그인 한 유저와 인자로 받은 유저가 일치하지 않으면, 권한 부족 예외 발생
+		if (!user.getId().equals(userId)) {
+			throw new CustomException(ErrorCode.FORBIDDEN_USER);
+		}
+
+		String key = imageUploadService.uploadImage(profileImage);
+		user.setProfileImage(key);
+
+		return ProfileImageResponseDto.builder()
+			.profileImage(imageUploadService.getImageUrl(key))
+			.build();
 	}
 }
