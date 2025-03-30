b0c5f3b7fda3f755cad0ca4bb1000fdbd1fb7763
조대성 <tfer2442@naver.com>
2025-03-30 21:04:04 +0900

    feat: Cursor 기반 User App Service 작성 (S12P21D211-167)

--- backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/user/service/UserAppService.java
@@ -4,7 +4,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.otoki.uptention.application.user.dto.request.JoinRequestDto;
 import com.otoki.uptention.application.user.dto.response.ProfileImageResponseDto;
+import com.otoki.uptention.application.user.dto.response.UserCursorResponseDto;
 import com.otoki.uptention.application.user.dto.response.UserResponseDto;
+import com.otoki.uptention.domain.user.enums.UserRole;
+import com.otoki.uptention.domain.user.enums.UserSortType;
 
 public interface UserAppService {
 	void joinMember(JoinRequestDto requestDto);
@@ -18,4 +21,7 @@ public interface UserAppService {
 	ProfileImageResponseDto removeProfileImage(Integer userId);
 
 	UserResponseDto getUser(Integer userId);
+
+	UserCursorResponseDto getUsers(UserRole userRole, String keyword, String cursorStr, UserSortType userSortType,
+		int size);
 }
