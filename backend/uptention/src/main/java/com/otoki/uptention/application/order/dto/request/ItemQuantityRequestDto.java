f27b30b3f38c50887dc1c1d6ceff264fad30ebec
ssuzyn <asj7090@gmail.com>
2025-03-30 19:00:42 +0900

    refactor: 요청 DTO 접근 제어 강화 (S12P21D211-220)

--- backend/uptention/src/main/java/com/otoki/uptention/application/order/dto/request/ItemQuantityRequestDto.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/order/dto/request/ItemQuantityRequestDto.java
@@ -1,15 +1,12 @@
 package com.otoki.uptention.application.order.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "상품 수량 요청 DTO")
 public class ItemQuantityRequestDto {
 
--- backend/uptention/src/main/java/com/otoki/uptention/application/order/dto/request/ItemVerificationDto.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/order/dto/request/ItemVerificationDto.java
@@ -1,15 +1,12 @@
 package com.otoki.uptention.application.order.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "구매할 상품 정보 DTO")
 public class ItemVerificationDto {
 	@Schema(description = "상품 ID", example = "1")
