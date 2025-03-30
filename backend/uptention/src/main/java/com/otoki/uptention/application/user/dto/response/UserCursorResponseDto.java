086cc76cf743062ef396c5d8ee1bfeed4d9aca03
조대성 <tfer2442@naver.com>
2025-03-30 21:03:12 +0900

    feat: User 커서 응답 dto 작성 (S12P21D211-167)

---
+++ backend/uptention/src/main/java/com/otoki/uptention/application/user/dto/response/UserCursorResponseDto.java
@@ -0,0 +1,25 @@
+package com.otoki.uptention.application.user.dto.response;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserCursorResponseDto {
+	@Schema(description = "상품 목록")
+	private List<UserResponseDto> users;
+
+	@Schema(description = "다음 페이지 존재 여부", example = "true")
+	private boolean hasNextPage;
+
+	@Schema(description = "다음 페이지 커서 값", example = "eyJ2YWx1ZSI6MTAwLCJpZCI6MX0=")
+	private String nextCursor;
+}
