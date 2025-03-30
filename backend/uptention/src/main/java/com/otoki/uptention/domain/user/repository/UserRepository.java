1ab91620916eae956e6f37e0bf2606d3b96a6532
조대성 <tfer2442@naver.com>
2025-03-30 15:37:20 +0900

    feat: 유저id와 회사정보로 User 조회하는 Repository 메서드 작성 (S12P21D211-167)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/user/repository/UserRepository.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/user/repository/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.otoki.uptention.domain.company.entity.Company;
 import com.otoki.uptention.domain.user.entity.User;
 
 @Repository
@@ -17,4 +18,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	// 로그인 아이디로 User 불러오기
 	Optional<User> findByUsername(String username);
+
+	// Id와 회사 정보로 User 불러오기
+	Optional<User> findByIdAndCompany(Integer id, Company company);
 }
