bd21fb6e97d8585161b390c6ac76edcf5971fc13
류재문 <woans5970@naver.com>
2025-03-31 09:44:47 +0900

    feat: 우수 사원 조회 쿼리 작성 (S12P21D211-141)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/mining/repository/MiningTimeRepository.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/mining/repository/MiningTimeRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.otoki.uptention.domain.mining.dto.response.MiningTimeRankResponseDto;
 import com.otoki.uptention.domain.mining.entity.MiningTime;
 import com.otoki.uptention.domain.user.entity.User;
 
@@ -38,5 +39,21 @@ public interface MiningTimeRepository extends JpaRepository<MiningTime, Integer>
 		@Param("startTime") LocalDateTime startTime,
 		@Param("endTime") LocalDateTime endTime
 	);
+
+	@Query(value = "SELECT u.name, " +
+		"       CAST(SUM(TIMESTAMPDIFF(MINUTE, m.start_time, m.end_time)) AS SIGNED) AS totalMinutes " +
+		"FROM mining_time m " +
+		"JOIN user u ON m.user_id = u.id " +
+		"WHERE m.start_time >= :startTime " +
+		"  AND m.end_time <= :endTime " +
+		"GROUP BY u.id " +
+		"ORDER BY totalMinutes DESC", nativeQuery = true)
+	List<MiningTimeRankResponseDto> findMiningTimeRanking(
+		@Param("startTime") LocalDateTime startTime,
+		@Param("endTime") LocalDateTime endTime
+	);
+
+
+
 
 }
