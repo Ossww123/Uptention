8466c9150d95fdb0a0e4fcb4dd24f8abf4a51626
류재문 <woans5970@naver.com>
2025-03-31 09:47:40 +0900

    feat: 우수 사원 조회 api 작성 (S12P21D211-141)

--- backend/uptention/src/main/java/com/otoki/uptention/presentation/mining/controller/MiningTimeController.java
+++ backend/uptention/src/main/java/com/otoki/uptention/presentation/mining/controller/MiningTimeController.java
@@ -1,32 +1,43 @@
 package com.otoki.uptention.presentation.mining.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.otoki.uptention.application.mining.service.MiningTimeAppService;
+import com.otoki.uptention.domain.mining.dto.response.MiningTimeRankResponseDto;
 import com.otoki.uptention.presentation.mining.doc.MiningApiDoc;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/focus")
+@RequestMapping("/api/mining-time")
 @RequiredArgsConstructor
 public class MiningTimeController implements MiningApiDoc {
 
 	private final MiningTimeAppService miningTimeAppService;
 
-	@PostMapping
+	@PostMapping("/focus")
 	public ResponseEntity<String> focusModeOn() {
 		miningTimeAppService.focusModeOn(1);
 		return ResponseEntity.ok("집중모드 시작");
 	}
 
-	@PatchMapping
+	@PatchMapping("/focus")
 	public ResponseEntity<String> focusModeOff() {
 		miningTimeAppService.focusModeOff(1);
 		return ResponseEntity.ok("집중모드 종료");
+	}
+
+	@GetMapping()
+	public ResponseEntity<Map<String, List<MiningTimeRankResponseDto>>> getMiningTimes(@RequestParam Integer top) {
+		return ResponseEntity.ok(miningTimeAppService.findMiningRank(top));
 	}
 }
