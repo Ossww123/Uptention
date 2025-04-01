737a1677417897571c34c83844eff3c8390d001d
류재문 <woans5970@naver.com>
2025-04-01 13:05:51 +0900

    feat: UptentionApplication 스케줄러 annotation 추가 (S12P21D211-248)

--- backend/uptention/src/main/java/com/otoki/uptention/UptentionApplication.java
+++ backend/uptention/src/main/java/com/otoki/uptention/UptentionApplication.java
@@ -3,11 +3,13 @@ package com.otoki.uptention;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.otoki.uptention.global.config.AwsS3Properties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AwsS3Properties.class)
+@EnableScheduling
 public class UptentionApplication {
 
 	public static void main(String[] args) {
