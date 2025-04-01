7e77a97458c0bfdab977dab6d67c508df8f9d5db
asj7090 <asj7090@gmail.com>
2025-04-01 15:55:42 +0900

    feat: 상품 카테고리 전체 조회 메서드 구현 (S12P21D211-225)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/category/service/CategoryService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/category/service/CategoryService.java
@@ -1,5 +1,11 @@
 package com.otoki.uptention.domain.category.service;
 
+import java.util.List;
+
+import com.otoki.uptention.domain.category.entity.Category;
+
 public interface CategoryService {
 	boolean isCategoryExists(Integer categoryId);
+
+	List<Category> getAllCategories();
 }
--- backend/uptention/src/main/java/com/otoki/uptention/domain/category/service/CategoryServiceImpl.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/category/service/CategoryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.otoki.uptention.domain.category.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.otoki.uptention.domain.category.entity.Category;
 import com.otoki.uptention.domain.category.repository.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,5 +23,13 @@ public class CategoryServiceImpl implements CategoryService {
 	@Override
 	public boolean isCategoryExists(Integer categoryId) {
 		return categoryRepository.existsById(categoryId);
+	}
+
+	/**
+	 * 모든 카테고리 정보 조회
+	 */
+	@Override
+	public List<Category> getAllCategories() {
+		return categoryRepository.findAll();
 	}
 }
