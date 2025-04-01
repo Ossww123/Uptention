acbcb1557bac696f16384dc0a026092fbc747bd8
ssuzyn <asj7090@gmail.com>
2025-04-02 02:28:17 +0900

    feat: 상품 정보 수정 API 구현 (S12P21D211-227)

--- backend/uptention/src/main/java/com/otoki/uptention/application/item/service/ItemAppService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/application/item/service/ItemAppService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.otoki.uptention.application.item.dto.request.ItemCreateRequestDto;
+import com.otoki.uptention.application.item.dto.request.ItemUpdateRequestDto;
 import com.otoki.uptention.application.item.dto.response.ItemCursorResponseDto;
 import com.otoki.uptention.application.item.dto.response.ItemResponseDto;
 import com.otoki.uptention.domain.item.entity.Item;
@@ -15,6 +16,8 @@ public interface ItemAppService {
 	Item createItem(ItemCreateRequestDto itemCreateRequestDto, List<MultipartFile> images);
 
 	void deleteItem(Integer itemId);
+
+	void updateItem(Integer itemId, ItemUpdateRequestDto itemUpdateRequestDto);
 
 	ItemResponseDto getItemDetails(Integer itemId);
 
--- backend/uptention/src/main/java/com/otoki/uptention/presentation/item/controller/ItemController.java
+++ backend/uptention/src/main/java/com/otoki/uptention/presentation/item/controller/ItemController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -65,6 +64,14 @@ public class ItemController implements ItemApiDoc {
 	public ResponseEntity<String> deleteItem(@PathVariable Integer itemId) {
 		itemAppService.deleteItem(itemId);
 		return ResponseEntity.ok("상품 삭제 완료");
+	}
+
+	@PatchMapping("/{itemId}")
+	public ResponseEntity<String> updateItem(
+		@PathVariable Integer itemId,
+		@RequestBody ItemUpdateRequestDto updateRequest) {
+		itemAppService.updateItem(itemId, updateRequest);
+		return ResponseEntity.ok("상품 수정 완료");
 	}
 
 }
