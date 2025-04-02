ebd7ebb4621c1b271e28d49a6de999921c8e9e9a
조대성 <tfer2442@naver.com>
2025-04-02 17:11:34 +0900

    feat: Fcm Domain Service 작성 (S12P21D211-177)

---
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/user/service/FcmTokenService.java
@@ -0,0 +1,10 @@
+package com.otoki.uptention.domain.user.service;
+
+import com.otoki.uptention.domain.user.entity.FcmToken;
+import com.otoki.uptention.domain.user.entity.User;
+
+public interface FcmTokenService {
+	void registerFcmToekn(FcmToken fcmToken);
+
+	void removeFcmToken(User user, String value);
+}
