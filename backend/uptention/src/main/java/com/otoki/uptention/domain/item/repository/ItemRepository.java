cad0d87cb6e7e22702b1819e514d555aac03a27b
조대성 <tfer2442@naver.com>
2025-04-04 16:17:44 +0900

    feat: Item 전체 조회 메서드 작성 at ItemRepository (S12P21D211-258)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/item/repository/ItemRepository.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/item/repository/ItemRepository.java
@@ -1,5 +1,6 @@
 package com.otoki.uptention.domain.item.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,4 +17,6 @@ public interface ItemRepository extends JpaRepository<Item, Integer>, ItemReposi
 	@Query("SELECT i FROM Item i LEFT JOIN FETCH i.images WHERE i.id = :itemId AND i.status = true")
 	Optional<Item> findActiveByIdWithImages(@Param("itemId") Integer itemId);
 
+	// 활성 상태인 모든 상품 조회
+	List<Item> findByStatusTrue();
 }
