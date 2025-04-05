b13c3c9d79a23a7c5f6545016a65d6ad4c3c5c7c
조대성 <tfer2442@naver.com>
2025-04-05 23:13:39 +0900

    feat: 알림 Entity 생성 (S12P21D211-177)

---
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/notification/entity/Notification.java
@@ -0,0 +1,40 @@
+package com.otoki.uptention.domain.notification.entity;
+
+import com.otoki.uptention.global.entity.TimeStampEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "`notification`")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Builder
+public class Notification extends TimeStampEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column
+	private String title;
+
+	@Column
+	private String message;
+
+	@Column
+	@Builder.Default
+	private Boolean read = false;
+}
