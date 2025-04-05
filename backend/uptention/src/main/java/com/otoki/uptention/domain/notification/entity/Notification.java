40935e4b8d350016b6ecbfaa9b8f85ef4b69dda2
조대성 <tfer2442@naver.com>
2025-04-05 23:26:41 +0900

    feat: Notification User 연관관계 추가 (S12P21D211-177)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/notification/entity/Notification.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/notification/entity/Notification.java
@@ -1,12 +1,16 @@
 package com.otoki.uptention.domain.notification.entity;
 
+import com.otoki.uptention.domain.user.entity.User;
 import com.otoki.uptention.global.entity.TimeStampEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -37,4 +41,8 @@ public class Notification extends TimeStampEntity {
 	@Column
 	@Builder.Default
 	private Boolean read = false;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 }
