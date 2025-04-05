37618e8042aeb84147d781fd6ffbfb7832296adb
류재문 <woans5970@naver.com>
2025-04-05 21:35:24 +0900

    feat: nft 생성 및 전송 스케줄러 구현 (S12P21D211-267)

--- backend/uptention/src/main/java/com/otoki/uptention/scheduler/MiningTimeScheduler.java
+++ backend/uptention/src/main/java/com/otoki/uptention/scheduler/MiningTimeScheduler.java
@@ -1,10 +1,13 @@
 package com.otoki.uptention.scheduler;
 
+import java.util.List;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.otoki.uptention.application.mining.service.MiningTimeAppService;
+import com.otoki.uptention.application.mining.service.MiningTimeAppServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,13 +20,22 @@ public class MiningTimeScheduler {
 	private final MiningTimeAppService miningTimeAppService;
 
 	@Transactional
-	@Scheduled(cron = "00 30 23 * * *", zone = "Asia/Seoul")
+	@Scheduled(cron = "00 30 11 * * *", zone = "Asia/Seoul")
 	public void updateNullEndTime() {
 		log.info("Updating null end time for scheduler");
 		miningTimeAppService.bulkUpdateMiningTime();
 		log.info("Updating user point for scheduler");
 		miningTimeAppService.bulkUpdateUserPoints();
 		log.info("Send Token for scheduler");
-		// miningTimeAppService.bulkSendToken();
+		miningTimeAppService.bulkSendToken();
+	}
+
+	@Transactional
+	@Scheduled(cron = "00 45 11 * * *", zone = "Asia/Seoul")
+	public void sendNft() {
+		log.info("Create NFT for scheduler");
+		List<MiningTimeAppServiceImpl.MintAddressResponse> mintAddressResponses = miningTimeAppService.bulkCreateNFT();
+		log.info("Send NFT for scheduler");
+		miningTimeAppService.bulkSendNFT(mintAddressResponses);
 	}
 }
