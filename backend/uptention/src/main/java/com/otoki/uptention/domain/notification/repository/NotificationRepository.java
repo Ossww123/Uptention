3a0f561335fbbabecdf8f498fb3b4397d84c4de2
조대성 <tfer2442@naver.com>
2025-04-05 23:38:22 +0900

    feat: 유저 안읽은 알림 모두 읽기 Repository 메서드 작성 (S12P21D211-177)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/notification/repository/NotificationRepository.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,18 @@
 package com.otoki.uptention.domain.notification.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.otoki.uptention.domain.notification.entity.Notification;
+import com.otoki.uptention.domain.user.entity.User;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+public interface NotificationRepository extends JpaRepository<Notification, Integer>, NotificationRepositoryCustom {
+	// 유저의 읽지 않은 알림을 모두 읽음 처리
+	@Modifying
+	@Query("UPDATE Notification n SET n.read = true WHERE n.user = :user AND n.read = false")
+	void markAllAsReadByUser(@Param("user") User user);
 }
