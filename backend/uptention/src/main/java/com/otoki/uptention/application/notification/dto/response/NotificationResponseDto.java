d73df7ee73bcf67865cec0968bf5977cbe4d599d
조대성 <tfer2442@naver.com>
2025-04-05 23:06:20 +0900

    feat: 알림 조회 Dto 작성 (S12P21D211-177)

---
+++ backend/uptention/src/main/java/com/otoki/uptention/application/notification/dto/response/NotificationResponseDto.java
@@ -0,0 +1,28 @@
+package com.otoki.uptention.application.notification.dto.response;
+
+import java.time.LocalDateTime;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(description = "알림 DTO")
+public class NotificationResponseDto {
+	@Schema(description = "알림 ID")
+	private Integer notificationId;
+
+	@Schema(description = "알림 제목")
+	private String title;
+
+	@Schema(description = "메시지")
+	private String message;
+
+	@Schema(description = "생성일자")
+	private LocalDateTime createdAt;
+}
