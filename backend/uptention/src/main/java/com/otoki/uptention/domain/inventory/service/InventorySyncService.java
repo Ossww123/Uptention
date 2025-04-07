28235d62e6c59d358b03115c7ad8ca1b32511c5d
조대성 <tfer2442@naver.com>
2025-04-07 16:40:10 +0900

    feat: bootstrap 분산락 적용 (S12P21D211-286)

--- backend/uptention/src/main/java/com/otoki/uptention/domain/inventory/service/InventorySyncService.java
+++ backend/uptention/src/main/java/com/otoki/uptention/domain/inventory/service/InventorySyncService.java
@@ -3,9 +3,5 @@ package com.otoki.uptention.domain.inventory.service;
 public interface InventorySyncService {
 	void initializeAllInventories();
 
-	// void syncInventoryToDatabase(Integer itemId);
-
 	void syncAllInventoriesToDatabase();
-
-	// void syncInventoryFromDatabase(Integer itemId);
 }
