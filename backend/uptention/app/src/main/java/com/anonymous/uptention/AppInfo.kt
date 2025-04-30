5961e68a9f608564cc293d83281a0c45098d7764
조대성 <tfer2442@naver.com>
2025-04-30 16:29:12 +0900

    refactor: 백엔드 폴더에 잘못들어온 app 관련 폴더 삭제

--- backend/uptention/app/src/main/java/com/anonymous/uptention/AppInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.anonymous.uptention
-
-data class AppInfo(
-    val packageName: String,
-    val appName: String,
-    val isSystemApp: Boolean = true,
-    var isAllowed: Boolean = false
-) 
